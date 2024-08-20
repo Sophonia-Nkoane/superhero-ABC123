57652ade7d87376b635cb4053c84c085c88e60f6
Sophonia Nkoane <snnkoane@gmail.com>
2024-08-20 20:11:50 +0200

    update

--- src/app/services/voice.service.ts
+++ src/app/services/voice.service.ts
@@ -4,9 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class VoiceService {
-  getUtterance(letter: string, language: string) {
-    throw new Error('Method not implemented.');
-  }
   voices: SpeechSynthesisVoice[] = [];
   selectedVoiceEnglish: SpeechSynthesisVoice | null = null;
   selectedVoiceAfrikaans: SpeechSynthesisVoice | null = null;
